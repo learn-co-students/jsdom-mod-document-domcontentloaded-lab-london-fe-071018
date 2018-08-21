e02a90136cc9f025a4827ef302ac2477ed12705c
clairemacdonald <claire.macdonald@blis.com>
2018-08-21 19:46:50 +0000

    Done.

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded" , e => {
+  
+  document.querySelector("body")
+  
+  .addEventListener("click", e => console.log("Reggae, Reggae!"));
+  
+  document.getElementById("hidden-div").innerHTML="301";
+ 
+});
