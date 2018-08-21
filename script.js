e03508f01e1cde87a781ea44dcd8d17e60a33ba8
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-21 19:53:12 +0000

    Done.

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", e => {
+  document.querySelector("body")
+  .addEventListener("click", e => console.log("Reggae, Reggae!"));
+  let values =document.getElementById('hidden-div').innerHTML='301';
+  
+})
+
